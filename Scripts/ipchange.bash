a078c21a2ff6185dcc18067a94e2369ab98bed8c
Joshua Capozzi <capozzijoshua03@gmail.com>
2023-12-23 11:19:58 +0800

    deleting secondary ip address

--- Scripts/ipchange.bash
+++ Scripts/ipchange.bash
@@ -9,6 +9,9 @@ read -p "Enter the DNS server IP address: " dns
 # Get the current network interface name (assuming it's eth0, change if needed)
 interface=$(ip route | grep default | awk '{print $5}')
 
+# Get the current secondary dynamic IP address (if any)
+secondary_ip=$(ip -4 addr show dev $interface | awk '/secondary/ {print $2}' | cut -d'/' -f1)
+
 # Backup the current network configuration file
 sudo cp /etc/netplan/01-netcfg.yaml /etc/netplan/01-netcfg.yaml.bak
 
@@ -29,6 +32,12 @@ EOL
 # Set secure permissions on the Netplan directory and configuration file
 sudo chmod 750 /etc/netplan
 sudo chmod 640 /etc/netplan/01-netcfg.yaml
+
+# Remove any secondary dynamic IP address
+if [ -n "$secondary_ip" ]; then
+  echo "Removing secondary IP address: $secondary_ip"
+  sudo ip addr del $secondary_ip/24 dev $interface
+fi
 
 # Apply the new configuration
 sudo netplan apply
